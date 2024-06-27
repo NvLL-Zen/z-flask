96105e8aedad24dacb74cfff62947d289c5bc22a
Zenriel <nvll.zen@gmail.com>
2024-06-27 14:05:33 +0700

    MALOAK

--- index.py
+++ index.py
@@ -4,12 +4,11 @@ import git
 app = Flask(__name__)
 
 @app.route('/update_server', methods=['POST'])
-def webhook():
+def update_server():
     if request.method == 'POST':
         repo = git.Repo('path/to/git_repo')
         origin = repo.remotes.origin
         origin.pull()
-
 
 @app.route('/')
 def hello():
