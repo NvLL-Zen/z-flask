1e95da0c2e384212b5c0cf30f83769d488526e05
Zenriel <nvll.zen@gmail.com>
2024-06-27 14:01:00 +0700

    POSTAOAOD

--- index.py
+++ index.py
@@ -3,21 +3,11 @@ import git
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/update_server', methods=['POST'])
 def webhook():
-    print("Webhook received")
     if request.method == 'POST':
-        try:
-            repo = git.Repo('/home/yourusername/yourproject')  # Replace with your project path
-            origin = repo.remotes.origin
-            origin.pull()
-            print("Repository updated successfully")
-            return '', 200
-        except Exception as e:
-            print(f"Error: {e}")
-            return '', 500
-    else:
-        return '', 400
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
 
 
 @app.route('/')
