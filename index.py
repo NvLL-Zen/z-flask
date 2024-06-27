3b3a6a61a1bd30c14edfcd2091ef716d332f9bbe
Zenriel <nvll.zen@gmail.com>
2024-06-27 14:42:07 +0700

    Added security measures

--- index.py
+++ index.py
@@ -1,15 +1,30 @@
-from flask import Flask, request ,render_template
+from flask import Flask, request, abort ,render_template
 import git
+import hmac
+import hashlib
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 app = Flask(__name__)
 
 @app.route('/update_server', methods=['POST'])
 def update_server():
     if request.method == 'POST':
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        if not is_valid_signature(x_hub_signature, request.data, "MULUTANDAKONTOL"):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(418)
         repo = git.Repo('/home/Zenriel/z-flask/')
         origin = repo.remotes.origin
         origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+        return 200
 
 @app.route('/')
 def hello():
